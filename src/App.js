f2d4ad898c6a87712973c8519187c44e66b6d5ed
sajid507 <sajidmunawar666@gmail.com>
2020-11-06 19:06:29 +0500

    Update App.js
    Data loading... showed

--- src/App.js
+++ src/App.js
@@ -6,19 +6,25 @@ function App() {
   let data={title:"waiting for data"}
 
   const [todo,setTodo]=useState(data)
+  const [isFetching,setFetching]=useState(true);
   useEffect(()=>{
 
     async function getData(){
-
+      setFetching(true);
       let response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
       console.log("response= ", response)
       const data = await response.json()
       setTodo(data);
+      setFetching(false);
       console.log("Data= ", data)
     }
     getData();
-  })
-
+  },[6])
+if(isFetching){
+  return <div>
+    Data Loading...
+  </div>
+}
 
 
   return (
